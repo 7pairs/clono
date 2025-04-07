305a3173e16b30f842bd0405d297a1f2e529e5ca
HASEBA Junya <7pairs@gmail.com>
2025-04-07 18:56:52 +0900

    Update core/-main

--- src/main/blue/lions/clono/core.cljs
+++ src/main/blue/lions/clono/core.cljs
@@ -12,7 +12,31 @@
 ; See the License for the specific language governing permissions and
 ; limitations under the License.
 
-(ns blue.lions.clono.core)
+(ns blue.lions.clono.core
+  (:require ["path" :as path]
+            [blue.lions.clono.analyze :as analyze]
+            [blue.lions.clono.file :as file]
+            [blue.lions.clono.parse :as parse]
+            [blue.lions.clono.render :as render]
+            [blue.lions.clono.transform :as transform]))
 
 (defn -main
-  [& args])
+  [& args]
+  (let [config (file/read-config-file (path/join js/__dirname "./config.edn"))
+        catalog (file/read-catalog-file (:catalog config))
+        manuscripts (file/read-markdown-files (:manuscripts config) catalog)
+        documents (parse/parse-manuscripts manuscripts
+                                           (parse/create-order-generator))
+        toc-items (analyze/create-toc-items documents)
+        heading-dic (analyze/create-heading-dic documents)
+        footnote-dic (analyze/create-footnote-dic documents)
+        indices (analyze/create-indices documents)
+        updated-documents (transform/transform-documents
+                           documents
+                           {:heading heading-dic :footnote footnote-dic})
+        updated-manuscripts (render/render-documents updated-documents)]
+    (file/write-markdown-files (:output config) updated-manuscripts)
+    (file/write-file (path/join (:output config) "toc.md")
+                     (render/render-toc toc-items))
+    (file/write-file (path/join (:output config) "index.md")
+                     (render/render-index-page indices))))
