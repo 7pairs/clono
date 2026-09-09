02fa5494a8a4a90c5313fbdd03944c6d47e1b4ee
HASEBA Junya <7pairs@gmail.com>
2026-09-09 19:55:43 +0900

    feat(reference): 単一ファイル用のプレースホルダーを実装

--- src/main/clono/transform/xref/table.cljs
+++ src/main/clono/transform/xref/table.cljs
@@ -3,4 +3,8 @@
 (def rule
   {:type "table"
    :class-names (fn [_target]
-                  ["clono-xref-table"])})
+                  ["clono-xref-table"])
+   :placeholder-texts
+   {"number" "表X.X"
+    "number-title" "表X.X 参照先未解決"
+    "title" "参照先未解決"}})
